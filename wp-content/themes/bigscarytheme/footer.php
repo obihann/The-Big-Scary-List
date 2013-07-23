f0cb4abae128d717243a5b5074bd95ab10b66df7
Jeff Hann <jeffhann@gmail.com>
2013-07-23 20:27:32 -0300

    adding registration

--- wp-content/themes/bigscarytheme/footer.php
+++ wp-content/themes/bigscarytheme/footer.php
@@ -6,6 +6,19 @@
 		<input type="hidden" name="redirect_to" value="<?php echo get_option('home'); ?>" />
 	</form>
 
+	<div id="registerForm"> <!-- Registration -->
+		<div id="register-form">
+			<form action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" method="post">
+				<label>Username:</label><input type="text" name="user_login" id="user_login" class="input" /><br /><br />
+				<label>Email:</label><input type="text" name="user_email" id="user_email" class="input"  /><br /><br />
+					<?php do_action('register_form'); ?>
+					<input type="submit" value="Register" id="register" />
+				<hr />
+				<p class="statement">Your password will be e-mailed to you.</p>
+			</form>
+		</div>
+	</div><!-- /Registration -->
+
 	<?php wp_footer(); ?>
 	</body>
 </html>
