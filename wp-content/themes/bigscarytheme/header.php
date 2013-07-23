47b1cb0da2c3c2974c287e2b6c0f9a69a018f877
Jeff Hann <jeff.hann@teamspace.ca>
2013-07-23 11:07:12 -0300

    adding author pages and restyling login

--- wp-content/themes/bigscarytheme/header.php
+++ wp-content/themes/bigscarytheme/header.php
@@ -10,3 +10,13 @@
 		<?php wp_head(); ?>
 	</head>
 <body>
+<?php if( !is_user_logged_in() ) : ?>
+	<article id="loginArea">
+		<span> Log In:</span>
+		<form method="post" action="<?php echo get_option('home'); ?>/wp-login.php">
+			<label>Username:</label><input name="log" type="text"><label>Password:</label><input name="pwd" type="password">
+			<input type="submit" value="Login">
+			<input type="hidden" name="redirect_to" value="<?php echo get_option('home'); ?>" />
+		</form>
+	</article>
+<?php endif; ?>
